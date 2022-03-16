800f7a50b7891ffffc5619573bfc33f7b55dd7cf
Tixon123 <84869623+Tixon123@users.noreply.github.com>
2022-03-16 14:53:41 +0400

    error

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("master2 brunch");
+        List<String> listOfStrings = new ArrayList<>();
+        listOfStrings.add("aaa");
+        listOfStrings.add("aaa");
+        listOfStrings.add("aaa");
+        listOfStrings.add("aaa");
+        for (int i = 0; i < listOfStrings.size(); i++) {
+            System.out.println(listOfStrings.get(i).charAt(i));
+        }
+
     }
+
 }
